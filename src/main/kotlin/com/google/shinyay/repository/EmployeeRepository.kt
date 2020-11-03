e6c7589f5f6da7839705e38fb3e8e3a2695e0a61
shinyay <shinya.com@gmail.com>
2020-11-03 18:45:21 +0900

    Add Scope as ApplicationScoped

--- src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
@@ -2,7 +2,9 @@ package com.google.shinyay.repository
 
 import com.google.shinyay.entity.Employee
 import io.quarkus.hibernate.orm.panache.kotlin.PanacheRepository
+import javax.enterprise.context.ApplicationScoped
 
 
+@ApplicationScoped
 class EmployeeRepository: PanacheRepository<Employee> {
 }
