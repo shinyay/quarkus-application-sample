bde144986747b31e1ef81db139e32761d4bd15cc
shinyay <shinya.com@gmail.com>
2020-11-03 18:54:54 +0900

    Add produce for resource as JSON format

--- src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
+++ src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
@@ -1,7 +1,10 @@
 package com.google.shinyay.resource
 
 import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
 
 @Path("/employees")
+@Produces(MediaType.APPLICATION_JSON)
 class EmployeeResource {
 }
