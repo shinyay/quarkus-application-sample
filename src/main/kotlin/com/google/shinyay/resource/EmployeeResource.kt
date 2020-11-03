3dc07810fb9f1e73ffd53c0cbc80a6040150d414
shinyay <shinya.com@gmail.com>
2020-11-03 18:55:20 +0900

    Add consume for resource as JSON format

--- src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
+++ src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
@@ -1,10 +1,12 @@
 package com.google.shinyay.resource
 
+import javax.ws.rs.Consumes
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
 @Path("/employees")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 class EmployeeResource {
 }
