5fb97a5e1a953a3f9d9d5cdb123fc1d93ffc9cc5
shinyay <shinya.com@gmail.com>
2020-11-06 19:51:21 +0900

    Add repository class at Constructor

--- src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
+++ src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
@@ -1,5 +1,6 @@
 package com.google.shinyay.resource
 
+import com.google.shinyay.repository.EmployeeRepository
 import javax.ws.rs.Consumes
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -8,5 +9,6 @@ import javax.ws.rs.core.MediaType
 @Path("/employees")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-class EmployeeResource {
+class EmployeeResource(val repository: EmployeeRepository) {
+
 }
