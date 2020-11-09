c4c9cb4caa902283e35fc1a5b9bf7a8aaef65364
shinyay <shinya.com@gmail.com>
2020-11-09 23:20:19 +0900

    Add javax.ws.rs.core.Application extended class and @OpenAPIDefinition

--- src/main/kotlin/com/google/shinyay/EmplyeeApp.kt
+++ src/main/kotlin/com/google/shinyay/EmplyeeApp.kt
@@ -1,7 +1,13 @@
 package com.google.shinyay
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition
+import org.eclipse.microprofile.openapi.annotations.info.Info
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import javax.ws.rs.core.Application
+
+@OpenAPIDefinition(info = Info(title = "Employee API", version = "1.0.0"))
+class EmployeeApplication: Application()
 
 val Any.logger: Logger
     get() = LoggerFactory.getLogger(this.javaClass)
