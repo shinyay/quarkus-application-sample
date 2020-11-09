2b1cbf34ed45eabd5baf509ed584258ddb9dc842
shinyay <shinya.com@gmail.com>
2020-11-09 09:37:55 +0900

    Add @ParamPath parameter to specify value

--- src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
+++ src/main/kotlin/com/google/shinyay/resource/EmployeeResource.kt
@@ -1,10 +1,8 @@
 package com.google.shinyay.resource
 
+import com.google.shinyay.entity.Employee
 import com.google.shinyay.repository.EmployeeRepository
-import javax.ws.rs.Consumes
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
 @Path("/employees")
@@ -14,4 +12,8 @@ class EmployeeResource(val repository: EmployeeRepository) {
 
     @GET
     fun findAll() = repository.listAll()
+
+    @GET
+    @Path("/{id}")
+    fun findById(@PathParam("id") id: Long): Employee? = repository.findById(id)
 }
