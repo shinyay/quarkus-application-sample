7e0d65b6bdcefb4369d31df0b374d6617308c56d
shinyay <shinya.com@gmail.com>
2020-11-10 23:21:20 +0900

    Add persist function test with POST

--- src/test/kotlin/EmployeeResourceTest.kt
+++ src/test/kotlin/EmployeeResourceTest.kt
@@ -1,5 +1,24 @@
+import com.google.shinyay.entity.Employee
 import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.junit.jupiter.api.Test
 
 @QuarkusTest
 class EmployeeResourceTest {
+
+    @Test
+    fun testAddEmployee() {
+        val employee = Employee(
+                firstName = "Alice",
+                lastName = "Liddel",
+                role = "Developer",
+                salary = 10000
+        )
+        given().body(employee).contentType(ContentType.JSON)
+                .post("/employees")
+                .then()
+                .statusCode(201)
+    }
 }
